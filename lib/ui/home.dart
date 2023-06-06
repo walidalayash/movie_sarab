a9d18e4371300b3277df95b877dc2021f9e91032
Walid Alayash <walidalayash99@gmail.com>
2023-06-06 12:19:53 +0200

    home page update

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+   Home({super.key});
+List movieItems = [
+  "أكشن","كوميديا","مغامرات","دراما","رعب","رياضة"
+];
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-     // appBar: AppBar(title: Text(''),),
      body:
     Column(children: [
       CarouselSlider(
@@ -18,16 +19,44 @@ class Home extends StatelessWidget {
       builder: (BuildContext context) {
         return Container(
           width: MediaQuery.of(context).size.width,
-          margin: EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: BoxDecoration(
-            color: Colors.amber
+          margin:const EdgeInsets.symmetric(horizontal: 5.0),
+          decoration:const BoxDecoration(
           ),
           child: Image.asset(i.toString()) 
         );
       },
     );
   }).toList(),
-)
+),
+Padding(
+  padding: const EdgeInsets.only(top: 20),
+  child:   SizedBox(
+    height: 40,
+    child:   ListView.builder(
+      itemCount: movieItems.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context,index) =>
+    Row(
+      children: [
+       const SizedBox(width: 20,),
+     
+    SizedBox(
+      height:35,
+      width: 50,
+      child: Container(
+        decoration: BoxDecoration(
+    color: Colors.amber,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(child: Text(movieItems[index].toString())),
+      ),
+    ),
+   ],
+    ),
+    ),
+  ),
+),
+
     ],)
     );
   }
